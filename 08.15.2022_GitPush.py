0a61d6e5dddddd4cd591efd07595bec7547c9e69
Dmitry Brown <dmitryabrown@gmail.com>
2022-08-29 21:57:12 -0400

    Update 08.15.2022_GitPush.py

--- 08.15.2022_GitPush.py
+++ 08.15.2022_GitPush.py
@@ -6,12 +6,12 @@
 
 import subprocess
 
-token='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'
+token='##################'
 
 subprocess.check_call(['git', 'add', '--all'], cwd='/root/GitHub/energynewz.github.io')
 subprocess.check_call(['git', 'commit', '-m', 'python push'], cwd='/root/GitHub/energynewz.github.io')
-subprocess.check_call(['git', 'push', 'https://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@github.com/DmitryBrown/energynewz.github.io', 'main'], cwd='/root/GitHub/energynewz.github.io')
+subprocess.check_call(['git', 'push', 'https://############@github.com/DmitryBrown/energynewz.github.io', 'main'], cwd='/root/GitHub/energynewz.github.io')
 
-# git push --set-upstream https://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@github.com/DmitryBrown/energynewz.github.io main
-# git push https://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@github.com/DmitryBrown/energynewz.github.io main
+# git push --set-upstream https://#############@github.com/DmitryBrown/energynewz.github.io main
+# git push https://#################i@github.com/DmitryBrown/energynewz.github.io main
 
